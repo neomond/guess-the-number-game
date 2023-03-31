6cb3deedcb0f9b4fd569a4c607c67ed86c461510
neomond <neoweise@yahoo.com>
2023-04-01 01:19:28 +0400

    Switching screens

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { View, TextInput, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -21,8 +21,9 @@ const StartGameScreen = () => {
         "Number has to be a number between 1 and 99.",
         [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
       );
+      return;
     }
-    return;
+    onPickNumber(chosenNumber);
   }
 
   return (
